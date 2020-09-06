afc886280c575a5b4e322e657f91c86385865dff
octaviocel <61020722+octaviocel@users.noreply.github.com>
2020-09-05 22:26:19 -0500

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -12,37 +12,37 @@ public class Encryption {
 	public static void main(String[] args) throws IOException {
 		
 		Scanner pen=new Scanner(System.in);
-		System.out.println("BIENVENIDO AL ENCRIPTADOR Y DESENCRIPTADOR");
+		System.out.println("BIENVENIDO AL ENCRIPTADOR Y DESENCRIPTADOR");   //Primer menu para decidir encriptar o desencriptar
 		System.out.println("[1]ENCRIPTAR      [2] DESENCRIPTAR");
 		int decide = pen.nextInt();
 		if(decide==1) {
-		System.out.println("Que deseas encriptar archivo o desde consola");
+		System.out.println("Que deseas encriptar archivo o desde consola");     //Menu si eliges encriptar
 		System.out.println("[1]consola   [2]archivo");
 
 		int deci1= pen.nextInt();
 		
 		if(deci1==1) {
 			System.out.println("Digita la clave de al menos 3 caracteres");
-			String clave = pen.next();
-			if(clave.length()>2) {
-				Listaencrip code=new Listaencrip();
-				for(int i=0;i<clave.length();i++) {
+			String clave = pen.next();					//Lee la clave como string
+			if(clave.length()>2) {	
+				Listaencrip code=new Listaencrip();		
+				for(int i=0;i<clave.length();i++) {			//mete el string en la lista code
 					int c = clave.charAt(i);
 					code.insertar(c);
 				}
 				System.out.println("Ingresa el mensaje a encriptar");
-				String mensaje = pen.next();
+				String mensaje = pen.next();				//lee el mensaje a encriptar
 				Listaencrip men=new Listaencrip();
 				for(int i=0;i<mensaje.length();i++) {
-					int m = mensaje.charAt(i);
+					int m = mensaje.charAt(i);			//lo convierte a lista
 					men.insertar(m);
 				}
 				Listaencrip resultadocon=new Listaencrip();
-				resultadocon=resultadocon.encriptar(men,code);
+				resultadocon=resultadocon.encriptar(men,code);		//llama a la funcion encriptar
 				System.out.println("El mesaje encriptado es");
 				resultadocon.recorrer();
 				System.out.println(" ");
-				String h =volver(resultadocon);
+				String h =volver(resultadocon);				//llama a la funcion volver para regresarlo a string
 				System.out.println(h);
 				
 			}
@@ -56,28 +56,28 @@ public class Encryption {
 					code.insertar(c);
 				}
 				System.out.println("Ingresa la ruta del archivo a encriptar ejemplo(C:\\Users\\DELL\\Desktop\\)");
-				String leer = pen.next();
+				String leer = pen.next();			//primero lee la ruta
 				System.out.println("Ingresa el nombre del archivo, ejemplo(input.txt)");
-				String nombre = pen.next();
-				File file =new File(leer+nombre);
+				String nombre = pen.next();			//despues lee el numbre y extencion del archivo
+				File file =new File(leer+nombre);		
 				FileReader reader = new FileReader (file);  
-				BufferedReader li = new BufferedReader(reader);
+				BufferedReader li = new BufferedReader(reader);	//abre el archivo para leerlo
 				
-				String linea1 = li.readLine();
+				String linea1 = li.readLine();			//pasa lo que tiene la primer linea a string
 				Listaencrip men=new Listaencrip();
-				for(int i = 0; i < linea1.length(); i++){	
+				for(int i = 0; i < linea1.length(); i++){	//convierte el string a lista
 		            int num = linea1.charAt(i);
 		            men.insertar(num);
 		        }
 				Listaencrip resultadocon=new Listaencrip();
-				resultadocon=resultadocon.encriptar(men,code);
-				String salida = System.getProperty("user.home")+"\\"+nombre;
+				resultadocon=resultadocon.encriptar(men,code);   //llama una lista nueva donde deposita lo que da encriptar
+				String salida = System.getProperty("user.home")+"\\"+nombre;  //mediante este metodo System.getProperty("user.home") busca la direccion del usuario
 				//resultadocon.recorrer();
-				System.out.println("Su archivo se guardo en "+salida);
-				String h =volver(resultadocon);
-				FileWriter fw=new FileWriter(salida);
+				System.out.println("Su archivo se guardo en "+salida);	
+				String h =volver(resultadocon);			//devuelve string la lista
+				FileWriter fw=new FileWriter(salida);		
 		        BufferedWriter bw=new BufferedWriter(fw);
-		        bw.write(h);
+		        bw.write(h);						//escribe en el archivo
 		        bw.flush();
 		        
 		        bw.close();
